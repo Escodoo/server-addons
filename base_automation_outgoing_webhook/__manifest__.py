19a7358c89fd9ee788be95cbe8e0f010b0b702dc
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-08-18 23:32:22 -0300

    [ADD] base_automation_outgoing_webhook

---
+++ base_automation_outgoing_webhook/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Base Automation Outgoing Webhook',
+    'summary': """
+        Base Automation Outgoing Webhook""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'website': 'https://www.escodoo.com.br',
+    'maintainers': ['marcelsavegnago'],
+    'images': ['static/description/banner.png'],
+    'category': 'Tools',
+    'depends': [
+        'base_automation',
+    ],
+    'demo': [
+        'demo/ir_actions_server.xml',
+    ],
+}
